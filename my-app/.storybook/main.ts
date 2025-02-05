424b3a41e63dfd5038ada29ebd566855a1fa078e
semilunaris <vlad.karpych@gmail.com>
2025-02-05 20:48:37 +0300

    Updates

---
+++ my-app/.storybook/main.ts
@@ -0,0 +1,21 @@
+import type { StorybookConfig } from "@storybook/react-webpack5";
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  staticDirs: ["../public"], // Путь к статическим файлам
+ 
+};
+
+export default config;
